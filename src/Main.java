c1bb2ee3b05e8e45d04143054d232bf889fc61e6
Francisco Naranjo <naranjofjx@gmail.com>
2024-10-28 11:34:03 -0400

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -11,33 +9,46 @@ public class Main {
     public static void main(String[] args) {
         Timer timer = new Timer();
         long delay = 0;
-        long period = 60000;
+        long period = 86400000L;
 
-        timer.scheduleAtFixedRate(new DeleteFilesTask(), delay, period);
+        timer.scheduleAtFixedRate(new DeleteFilesTask("C:\\Windows\\Temp"), delay, period);
+        timer.scheduleAtFixedRate(new DeleteFilesTask(System.getenv("TEMP")), delay, period);
     }
 }
 
 class DeleteFilesTask extends TimerTask {
+    private final String folderPath;
+
+    public DeleteFilesTask(String folderPath) {
+        this.folderPath = folderPath;
+    }
+
     @Override
     public void run() {
-        String folderPath = "C:\\Windows\\Temp";
         Path path = Paths.get(folderPath);
 
         try {
             if (Files.exists(path)) {
-                try (Stream<Path> stream = Files.list(path)) {
-                    List<Path> files = stream.filter(Files::isRegularFile).toList();
+                try (Stream<Path> stream = Files.walk(path)) {
+                    List<Path> files = stream.sorted((p1, p2) -> p2.compareTo(p1))
+                            .filter(p -> Files.isRegularFile(p) || Files.isDirectory(p))
+                            .toList();
 
                     files.forEach(file -> {
                         try {
-                            Files.delete(file);
-                            System.out.println("Archivo eliminado: " + file);
+                            if (Files.isDirectory(file)) {
+                                Files.delete(file);
+                                System.out.println("Carpeta eliminada: " + file);
+                            } else {
+                                Files.delete(file);
+                                System.out.println("Archivo eliminado: " + file);
+                            }
                         } catch (IOException e) {
-                            System.out.println("No se puede eliminar el archivo: " + file);
+                            System.out.println("No se puede eliminar: " + file);
                         }
                     });
 
-                    System.out.println("Los archivos han sido eliminados.");
+                    System.out.println("Los archivos y carpetas han sido eliminados.");
                 }
             } else {
                 System.out.println("La carpeta no existe: " + folderPath);
