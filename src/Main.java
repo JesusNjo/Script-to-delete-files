d2b1cc27b436113c45b3ca82c960b6abf3564b81
Francisco Naranjo <naranjofjx@gmail.com>
2024-10-28 11:28:57 -0400

    test

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,41 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
+        Timer timer = new Timer();
+        long delay = 0;
+        long period = 60000;
+
+        timer.scheduleAtFixedRate(new DeleteFilesTask(), delay, period);
+    }
+}
+
+class DeleteFilesTask extends TimerTask {
+    @Override
+    public void run() {
         String folderPath = "C:\\Windows\\Temp";
         Path path = Paths.get(folderPath);
 
         try {
             if (Files.exists(path)) {
                 try (Stream<Path> stream = Files.list(path)) {
-                    List<Path> files = stream.filter(Files::isRegularFile)
-                            .toList();
+                    List<Path> files = stream.filter(Files::isRegularFile).toList();
 
-                    files.forEach(file->{
-                        try{
+                    files.forEach(file -> {
+                        try {
                             Files.delete(file);
                             System.out.println("Archivo eliminado: " + file);
-                        }catch (Exception e){
-                            System.out.println("No se puede eliminar el archivo: "+file);
+                        } catch (IOException e) {
+                            System.out.println("No se puede eliminar el archivo: " + file);
                         }
                     });
-                    System.out.println("Los archivos han sido listados.");
+
+                    System.out.println("Los archivos han sido eliminados.");
                 }
             } else {
                 System.out.println("La carpeta no existe: " + folderPath);
